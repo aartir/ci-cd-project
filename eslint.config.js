ee3cec2e6861e4aafaa2d9ef1c466f9c54bceedd
aartir <aarti459510@gmail.com>
2025-12-23 10:46:15 +0530

    Trigger CI

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs"
+    },
+    rules: {
+      semi: ["error", "always"]
+    }
+  }
+];
--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.get('/', (req, res) => {
     res.json({
         message: "CI/CD pipeling is working"
     });
-})
+});
 
 
 if (require.main === 'module') {
     app.listen(PORT, () => {
-        console.log(`listening on port ${PORT}`)
-    })
+        console.log(`listening on port ${PORT}`);
+    });
 }
 
 module.exports = app;
